cbe56ed0ce7678b1bb7061b399954d6cd7a92645
'TommyKim74' <'kims197411@gmail.com'>
2023-08-10 21:30:29 +0900

    first commit

---
+++ src/main/java/kr/co/trito/CicdWebProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.trito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdWebProjectApplication.class, args);
+	}
+
+}
